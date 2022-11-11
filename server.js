bf07877a42e4e5f6f75e9eb76ce67abf8803c43d
== <zaheerpersonal5@gmail.com>
2022-11-11 17:41:47 +0530

    first commit

--- server.js
+++ server.js
@@ -4,17 +4,27 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+let arr = [];
 app.use(express.static(__dirname));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 io.on("connection", (socket) => {
-  console.log("a user connected");
   socket.on("chat-message", (name, value) => {
     console.log(name, value);
     socket.broadcast.emit("append-messages", name, value);
   });
+  socket.on("connected-user", (name) => {
+    arr.push({ id: socket.id, name });
+    socket.broadcast.emit("connecteduser", arr);
+    socket.emit(
+      "myuser",
+      arr.filter((obj) => obj.id != socket.id)
+    );
+    console.log(arr);
+  });
 });
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("listening on *:3000");
--- styles/app.js
+++ styles/app.js
@@ -1,3 +1,47 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+const socket = io();
+let name = prompt("enter your name !!");
+const messages = document.getElementsByClassName("messages-block")[0];
+const users = document.getElementsByClassName("connected-user")[0];
+while (!name) name = prompt("enter your name");
+socket.on("connect", () => {
+  socket.emit("connected-user", name);
+});
+socket.on("connecteduser", (arr) => {
+  console.log("event triggerd");
+  arr.map((obj) => {
+    const div = document.createElement("div");
+    div.innerHTML =
+      obj.name +
+      '  <i class="fa-solid fa-user"></i>' +
+      ' : <bold style="color: darkgreen; font-weight:700">online</bold> <i class="fa-solid fa-check"></i>';
+    div.style.marginBottom = "10px";
+    users.append(div);
+  });
+});
+socket.on("runuser", (arr) => {
+  console.log("someuser is connected");
+  console.log(arr);
+});
 
-const socket = io("http://localhost:3000");
+const addUp = (name, value) => {
+  const div = document.createElement("div");
+  div.innerHTML = name + '  <i class="fa-solid fa-user"></i>' + " : " + value;
+  messages.append(div);
+  div.style.marginBottom = "10px";
+  window.scrollTo(0, div.scrollHeight);
+};
+
+const input = document.getElementById("msg");
+document.getElementById("basic-addon2").addEventListener("click", (evt) => {
+  if (input.value) {
+    socket.emit("chat-message", name, input.value);
+    addUp("You ", input.value);
+    input.value = "";
+  }
+});
+
+socket.on("append-messages", (name, value) => {
+  addUp(name, value);
+  console.log("added");
+});
